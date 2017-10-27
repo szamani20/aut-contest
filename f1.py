03532ffa42ce7a7c8bc3d5dcbc8e79b9105c1bbd
mohammadreza <daneshvaramoli@ce.sharif.edu>
2017-10-27 21:27:05 +0330

    Update f1.py

--- f1.py
+++ f1.py
@@ -16,19 +16,18 @@ def transaction(cid, aid,
     # i_transaction_amount = float(tamount[:-4])
     i_transaction_remain = float(tremain[:-4])
     p = Point(tdate, ttime, i_transaction_remain)
-    a = Account(aid)
+
     if aid not in list_of_accounts:
-        list_of_accounts[aid] = a
-    else:
-        a = list_of_accounts[aid]
-    a.points[p.transaction_datetime] = p
+        list_of_accounts[aid] = Account(aid)
+
+    list_of_accounts[aid].points[p.transaction_datetime] = p
     c = Customer(cid)
     if cid not in list_of_customers:
         list_of_customers[cid] = c
     else:
         c = list_of_customers[cid]
     if aid not in c.accounts:
-        c.accounts[aid] = a
+        c.accounts[aid] = list_of_accounts[aid]
 
 
 class Account:
@@ -85,7 +84,7 @@ class Point:
         self.remain = remain
 
     def seconds_from_begin(self):
-        return (self.transaction_datetime - dt.datetime.utcfromtimestamp(0)).total_seconds()
+        return (self.transaction_datetime - dt.datetime.utcfromtimestamp(1284286794)).total_seconds()
 
     def __hash__(self):
         return hash(self.transaction_datetime.ctime())
@@ -95,9 +94,15 @@ class Node:
     def __init__(self, aid, num):
         self.account_id = aid
         self.mnum = num
-        #self.points = dict()
-        self.balance_avg = integral_func(aid)
-        self.balance_std = std_func(aid)
+        # self.points = dict()
+        # self.balance_std = std_func(aid, self.balance_avg)
+        if len(list_of_accounts[aid].points) == 0:
+            self.balance_avg = list_of_accounts[aid].nodes[0].balance_avg
+            self.balance_std = list_of_accounts[aid].nodes[0].balance_std
+        else:
+            self.balance_avg = integral_func(aid)
+            self.balance_std = std_func_discrete(aid)
+
 
 def show_plot(account_id):
     x_data = []
@@ -107,14 +112,11 @@ def show_plot(account_id):
         print('Account ID invalid')
         return
 
-        # for p in list_of_accounts[account_id].points:
-    a = list_of_accounts[account_id]
-    list_of_points = sorted(a.allpoints.values(), key=lambda x: x.transaction_datetime)
+    list_of_points = sorted(list_of_accounts[account_id].allpoints.values(), key=lambda x: x.transaction_datetime)
 
     for p in list_of_points:
         x_data.append(p.transaction_datetime)
         y_data.append(p.remain)
-        # print(p.transaction_datetime, p.remain)
 
     plt.xlabel('Date')
     plt.plot_date(x_data,
@@ -135,169 +137,77 @@ def extract_points_from_aid(aid):
     for point in sorted(list_of_accounts[aid].points.values(), key=lambda p: p.transaction_datetime):
         x_val = point.seconds_from_begin()
         y_val = point.remain
-        if x_val > max_x:
-            max_x = x_val
-        if x_val < min_x:
-            min_x = x_val
+        max_x = max(x_val, max_x)
+        min_x = min(x_val, min_x)
         x.append(x_val)
         y.append(y_val)
 
-    x = np.array(x)
-    y = np.array(y)
-
-    return x, y, min_x, max_x
+    return np.array(x), np.array(y), min_x, max_x
 
 
 def integral_func(aid):
     x, y, min_x, max_x = extract_points_from_aid(aid)
+    if len(x) == 1 or len(y) == 1:
+        return y
     f = InterpolatedUnivariateSpline(x, y, k=1)
     return f.integral(min_x, max_x) / (max_x - min_x)
 
 
-def std_func(aid):
+def std_func(aid, balance):
     x, y, min_x, max_x = extract_points_from_aid(aid)
-    # TODO: X or Y ???
-    # FIXME
-    return np.std(np.array(y))
+    if len(x) == 1 or len(y) == 1:
+        return 0
+    y2 = []
+    for yy in y:
+        y2.append((yy - balance) ** 2)
+
+    f = InterpolatedUnivariateSpline(x, y2, k=1)
+    return f.integral(min_x, max_x) / (max_x - min_x)
+
+
+def std_func_discrete(aid):
+    x, y, min_x, max_x = extract_points_from_aid(aid)
+    return np.std(y)
+
+
+def read_month(month_number):
+    filename = 'month_' + str(month_number) + '.csv'
+    df = pd.read_csv(filename,
+                     # nrows=10000,
+                     index_col=[0])
+    for index, row in df.iterrows():
+        transaction(index,
+                    row['accountId'],
+                    row['transactionDate'],
+                    row['transactionTime'],
+                    row['terminalId'],
+                    row['transactionAmount'],
+                    row['transactionRemain'],
+                    row['state'],
+                    row['transactionCode'])
+
+    for aa in list_of_accounts.values():
+        nnode = Node(aa.account_id, month_number)
+        aa.nodes.append(nnode)
+        aa.allpoints.update(aa.points)
+        aa.points = dict()
 
 
 now = int(round(time.time()))
 list_of_accounts = dict()
 list_of_customers = dict()
 
-filename = 'month_1.csv'
-df = pd.read_csv(filename,
-                 index_col=[0])
-pp = 0
-print(int(round(time.time())) - now)
-pm = 1
-for index, row in df.iterrows():
-    pp += 1
-    transaction(index,
-                row['accountId'],
-                row['transactionDate'],
-                row['transactionTime'],
-                row['terminalId'],
-                row['transactionAmount'],
-                row['transactionRemain'],
-                row['state'],
-                row['transactionCode'])
+for i in range(1, 6):
+    print(i)
+    read_month(i)
+    print("read_month : " + str(int(round(time.time())) - now))
 
-for a in list_of_accounts:
-    aa = list_of_accounts[a]
-    nnode = Node(aa.account_id, pm)
-    aa.nodes.append(nnode)
-    aa.allpoints.update(aa.points)
-    aa.points = dict()
+sample_accounts = ['zhanghu_51320',
+                   # 'zhanghu_51318',
+                   # 'zhanghu_51319'
+                   ]
 
-filename = 'month_2.csv'
-df = pd.read_csv(filename,
-                 index_col=[0])
-print(int(round(time.time())) - now)
-pm = 2
-for index, row in df.iterrows():
-    pp += 1
-    transaction(index,
-                row['accountId'],
-                row['transactionDate'],
-                row['transactionTime'],
-                row['terminalId'],
-                row['transactionAmount'],
-                row['transactionRemain'],
-                row['state'],
-                row['transactionCode'])
-
-for a in list_of_accounts:
-    aa = list_of_accounts[a]
-    nnode = Node(aa.account_id, pm)
-    aa.nodes.append(nnode)
-    aa.allpoints.update(aa.points)
-    aa.points = dict()
-
-filename = 'month_3.csv'
-df = pd.read_csv(filename,
-                 index_col=[0])
-print(int(round(time.time())) - now)
-pm = 3
-for index, row in df.iterrows():
-    pp += 1
-    transaction(index,
-                row['accountId'],
-                row['transactionDate'],
-                row['transactionTime'],
-                row['terminalId'],
-                row['transactionAmount'],
-                row['transactionRemain'],
-                row['state'],
-                row['transactionCode'])
-
-for a in list_of_accounts:
-    aa = list_of_accounts[a]
-    nnode = Node(aa.account_id, pm)
-    aa.nodes.append(nnode)
-    aa.allpoints.update(aa.points)
-    aa.points = dict()
-
-filename = 'month_4.csv'
-df = pd.read_csv(filename,
-                 index_col=[0])
-print(int(round(time.time())) - now)
-pm = 4
-for index, row in df.iterrows():
-    pp += 1
-    transaction(index,
-                row['accountId'],
-                row['transactionDate'],
-                row['transactionTime'],
-                row['terminalId'],
-                row['transactionAmount'],
-                row['transactionRemain'],
-                row['state'],
-                row['transactionCode'])
-
-for a in list_of_accounts:
-    aa = list_of_accounts[a]
-    nnode = Node(aa.account_id, pm)
-    aa.nodes.append(nnode)
-    aa.allpoints.update(aa.points)
-    aa.points = dict()
-
-filename = 'month_5.csv'
-df = pd.read_csv(filename,
-                 index_col=[0])
-print(int(round(time.time())) - now)
-pm = 5
-for index, row in df.iterrows():
-    pp += 1
-    transaction(index,
-                row['accountId'],
-                row['transactionDate'],
-                row['transactionTime'],
-                row['terminalId'],
-                row['transactionAmount'],
-                row['transactionRemain'],
-                row['state'],
-                row['transactionCode'])
-
-for a in list_of_accounts:
-    aa = list_of_accounts[a]
-    nnode = Node(aa.account_id)
-    aa.nodes.append(nnode)
-    aa.allpoints.update(aa.points)
-    aa.points = dict()
-
-print("pp: " + str(pp / 1000) + " time: " + str(int(round(time.time())) - now))
-
-print(integral_func('zhanghu_51317'))
-print(std_func(('zhanghu_51317')))
-show_plot('zhanghu_51317')
-
-print(integral_func('zhanghu_51318'))
-print(std_func(('zhanghu_51318')))
-show_plot('zhanghu_51318')
-
-print(integral_func('zhanghu_51319'))
-print(std_func(('zhanghu_51319')))
-show_plot('zhanghu_51319')
+for a in sample_accounts:
+    show_plot(a)
 
 print("END time: " + str(int(round(time.time())) - now))
